dbfc2901de065b643075bc2315d4411e9c9af2a3
Olivier gg <ggo98@outlook.com>
2025-01-06 10:55:13 +0100

    feature/possibilty-to-select-resolution

--- WebcamExample/Form1.Designer.cs
+++ WebcamExample/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,12 +83,26 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.IndianRed;
+            this.button4.Location = new System.Drawing.Point(255, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 36);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "GC";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(660, 552);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
@@ -106,6 +121,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
---
+++ WebcamExample/FormSelectResolution.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WebcamExample
+{
+    partial class FormSelectResolution
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.IDOK = new System.Windows.Forms.Button();
+            this.IDCANCEL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(503, 384);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // IDOK
+            // 
+            this.IDOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.IDOK.Location = new System.Drawing.Point(360, 416);
+            this.IDOK.Name = "IDOK";
+            this.IDOK.Size = new System.Drawing.Size(75, 23);
+            this.IDOK.TabIndex = 1;
+            this.IDOK.Text = "OK";
+            this.IDOK.UseVisualStyleBackColor = true;
+            this.IDOK.Click += new System.EventHandler(this.IDOK_Click);
+            // 
+            // IDCANCEL
+            // 
+            this.IDCANCEL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.IDCANCEL.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.IDCANCEL.Location = new System.Drawing.Point(441, 417);
+            this.IDCANCEL.Name = "IDCANCEL";
+            this.IDCANCEL.Size = new System.Drawing.Size(75, 23);
+            this.IDCANCEL.TabIndex = 2;
+            this.IDCANCEL.Text = "Cancel";
+            this.IDCANCEL.UseVisualStyleBackColor = true;
+            this.IDCANCEL.Click += new System.EventHandler(this.IDCANCEL_Click);
+            // 
+            // FormSelectResolution
+            // 
+            this.AcceptButton = this.IDOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.IDCANCEL;
+            this.ClientSize = new System.Drawing.Size(528, 455);
+            this.Controls.Add(this.IDCANCEL);
+            this.Controls.Add(this.IDOK);
+            this.Controls.Add(this.listBox1);
+            this.Name = "FormSelectResolution";
+            this.Text = "FormSelectResolution";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button IDOK;
+        private System.Windows.Forms.Button IDCANCEL;
+    }
+}
